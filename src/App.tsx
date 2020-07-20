8322e3cc4eb92c6822401abac1ae67f19d856b47
Samuel Meira <samuelleandmeira@hotmail.com>
2020-07-20 20:11:17 -0300

    setup global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
 
 const App: React.FC = () => {
-  return <div />;
+  return (
+    <>
+      <GlobalStyle />
+      <h1>Hello World</h1>
+    </>
+  );
 };
 
 export default App;
