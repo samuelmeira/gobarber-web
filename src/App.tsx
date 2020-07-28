8e354238cd995296c4f6ad13c426fa2f693b3391
Samuel Meira <samuelleandmeira@hotmail.com>
2020-07-27 23:27:13 -0300

    add and remove toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
+// import SignUp from './pages/SignUp';
 
 import AppProvider from './hooks';
 
