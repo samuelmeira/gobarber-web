8111bdd84d85ce3cd078d79f0d4346d0320f2a5d
Samuel Meira <samuelleandmeira@hotmail.com>
2020-07-28 21:31:52 -0300

    refactor routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+
+import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+import Dashboard from '../pages/Dashboard';
 
 // import { Container } from './styles';
 
@@ -11,6 +14,8 @@ const Routes: React.FC = () => {
     <Switch>
       <Route path="/" exact component={SignIn} />
       <Route path="/signup" exact component={SignUp} />
+
+      <Route path="/dashboard" exact isPrivate component={Dashboard} />
     </Switch>
   );
 };
